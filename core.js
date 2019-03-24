71c370530c59fafb06780c04f2d4c723b5112ab6
shandyCode28 <48887336+shandyCode28@users.noreply.github.com>
2019-03-24 23:35:16 +0700

    Update core.js

--- core.js
+++ core.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-
+	/*
 	$('.konten').remove();
 	$('body').css("background", "black");
 	$('span').text('IRSHANDY JUNIAR HARDADI')
-	$('body').append("<h1 style='font-size:7vw;color:green'> SCRIPT TELAH DINONACKTIFKAN OLEH IRSHANDY JUNIAR HARDADI KARENA ALASAN KEAMANAN <br> SILAHKAN CHAT AJA YANG EMANG ORDER SECARA RESMI</h1><h3 style='font-size:7vw;color:red'> DAN UNTUK LU YANG NGEKREK GW UDAH LACAK LU ANJING TUNGGU AJA BOSS</h3>")
+	$('body').append("<h1 style='font-size:7vw;color:green'> SCRIPT TELAH DINONACKTIFKAN OLEH IRSHANDY JUNIAR HARDADI KARENA ALASAN KEAMANAN <br> SILAHKAN CHAT AJA YANG EMANG ORDER SECARA RESMI</h1><h3 style='font-size:7vw;color:red'> DAN UNTUK LU YANG NGEKREK GW UDAH LACAK LU ANJING TUNGGU AJA BOSS</h3>")*/
 
 })
